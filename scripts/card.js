5e27ea1d889a11c2fad2e354fe8920b5338211fa
mikan7296 <dumbmonkey10@gmail.com>
2023-01-31 05:51:03 +0800

    fix card template literal values

--- scripts/card.js
+++ scripts/card.js
@@ -1,40 +1,19 @@
-
 $(document).ready(function () {
     for (var i=0;i<20;i++) {
 
         let card = 
-            `<div id="card" class="container px-2 h-fit">
+            `<div id="card_${i}" class="container px-2 h-fit">
                 <div class="m-2 mt-8 p-2 h-72 bg-orange-300 rounded-lg shadow-xl">
                     <img class="w-full h-3/5 object-fill" src="../media/burger.jfif">
                     <div class="h-2/5 grid grid-rows-2">
-                        <h1 class="text-lg font-semibold truncate">NAMENAMENAMENAMENAMENAMENAMENAME</h1>
-                        <h3>$123.45</h3>
+                        <h1 id="name_${i}" class="text-lg font-semibold truncate">NAME_${i}</h1>
+                        <h3 id="price_${i}" >$123.45</h3>
                     </div>
                 </div>
             </div>`
 
-
         container = $("#card-container")
         container.append(card)
     }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
 });
 
