9681a6a6b94db4a7b71072ec70ab3647bcb17ff5
Daron Tan <s10242370@connect.np.edu.sg>
2023-02-08 16:07:36 +0800

    fix filter tags lowercasing

--- scripts/filter.js
+++ scripts/filter.js
@@ -1,88 +1,93 @@
 const filters = {
-    "gibson" : false,
-    "epiphone" : false,
-    "yamaha" : false
-}
+	gibson: false,
+	epiphone: false,
+	yamaha: false,
+};
 
-$(document).ready(function() {
-    $("#category-dropdown-brands").click(function (e){
-        $("#category-dropdown-icon-brands").toggleClass("rotate-180")
-        $("#category-container-brands").toggleClass("max-h-0 max-h-40")
-    })
+$(document).ready(function () {
+	$("#category-dropdown-brands").click(function (e) {
+		$("#category-dropdown-icon-brands").toggleClass("rotate-180");
+		$("#category-container-brands").toggleClass("max-h-0 max-h-40");
+	});
 
-    $("#search").keyup(function(){
-        let string = $("#search").val()
-        getCards(string)
-    })
+	$("#search").keyup(function () {
+		let string = $("#search").val();
+		getCards(string);
+	});
 
-    $(".brand-filter-option").click(function(e){
-        let brand = $(this).attr('data-brand')
-        filter(brand)
-    })
+	$(".brand-filter-option").click(function (e) {
+		let brand = $(this).attr("data-brand");
+		filter(brand);
+	});
 });
 
-function getCards (string) {
-    let container = $("#card-container")
-    let totalItemsLength =  container.find("h1").length
-    for (let i = 0;i<totalItemsLength;i++) {
-        if ($(`.forNameSearch_${i}`).text().toUpperCase().includes(string.toUpperCase())) {
-            $(`#card_${i}`).removeClass("invisible")
-            $(`#card_${i}`).css("order",0)
-        } else {
-            $(`#card_${i}`).addClass("invisible")
-            $(`#card_${i}`).css("order",1)
-        }
-    }
-    container.animate({scrollTop:0},300)
+function getCards(string) {
+	let container = $("#card-container");
+	let totalItemsLength = container.find("h1").length;
+	for (let i = 0; i < totalItemsLength; i++) {
+		if (
+			$(`.forNameSearch_${i}`)
+				.text()
+				.toUpperCase()
+				.includes(string.toUpperCase())
+		) {
+			$(`#card_${i}`).removeClass("invisible");
+			$(`#card_${i}`).css("order", 0);
+		} else {
+			$(`#card_${i}`).addClass("invisible");
+			$(`#card_${i}`).css("order", 1);
+		}
+	}
+	container.animate({ scrollTop: 0 }, 300);
 }
 
 function filter(name) {
-    if (filters[name]) {
-        filters[name] = false
-    } else {
-        filters[name] = true
-    }
-    let toShow = []
-    let toHide = []
-    for (let k in filters) {
-        let v = filters[k]
-        if (v) {
-            toShow.push(k)
-        } else {
-            toHide.push(k)
-        }
-    }
+	if (filters[name]) {
+		filters[name] = false;
+	} else {
+		filters[name] = true;
+	}
+	let toShow = [];
+	let toHide = [];
+	for (let k in filters) {
+		let v = filters[k];
+		if (v) {
+			toShow.push(k.toLowerCase());
+		} else {
+			toHide.push(k.toLowerCase());
+		}
+	}
 
-    let container = $("#card-container")
-    let totalItemsLength =  container.find("h1").length
-    for (let i = 0;i<totalItemsLength;i++) {
-        let tags = $(`#card_${i}`).attr('data-tags').split(',')
-        let show = false
-        for (let k in tags) {
-            let v = tags[k]
-            if (toShow.includes(v)) {
-                show = true
-            }
-        }
-        if (show) {
-            $(`#card_${i}`).removeClass("invisible")
-            $(`#card_${i}`).css("order",0)
-        } else {
-            $(`#card_${i}`).addClass("invisible")
-            $(`#card_${i}`).css("order",1)
-        }
-    }
+	let container = $("#card-container");
+	let totalItemsLength = container.find("h1").length;
+	for (let i = 0; i < totalItemsLength; i++) {
+		let tags = $(`#card_${i}`).attr("data-tags").split(",");
+		let show = false;
+		for (let k in tags) {
+			let v = tags[k];
+			if (toShow.includes(v.toLowerCase())) {
+				show = true;
+			}
+		}
+		if (show) {
+			$(`#card_${i}`).removeClass("invisible");
+			$(`#card_${i}`).css("order", 0);
+		} else {
+			$(`#card_${i}`).addClass("invisible");
+			$(`#card_${i}`).css("order", 1);
+		}
+	}
 
-    if (toShow.length == 0) {
-        showAll()
-    }
+	if (toShow.length == 0) {
+		showAll();
+	}
 }
 
 function showAll() {
-    let container = $("#card-container")
-    let totalItemsLength =  container.find("h1").length
-    for (let i = 0;i<totalItemsLength;i++) {
-        $(`#card_${i}`).removeClass("invisible")
-        $(`#card_${i}`).css("order",0)
-    }
+	let container = $("#card-container");
+	let totalItemsLength = container.find("h1").length;
+	for (let i = 0; i < totalItemsLength; i++) {
+		$(`#card_${i}`).removeClass("invisible");
+		$(`#card_${i}`).css("order", 0);
+	}
 }
